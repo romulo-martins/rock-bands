169856166187b84a68601ab8534e33c9356266df
romulo.martins <romulo_martins@id.uff.br>
2016-11-08 16:11:45 -0200

    Modificando o seed para adicionar tambem imagens

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,40 +6,48 @@ Band.create!([
 	biography: "Megadeth é uma banda norte-americana de heavy metal liderada por seu fundador, o vocalista e guitarrista Dave Mustaine. O grupo foi formado em 1983, após Dave ser demitido do Metallica. Desde então, a banda lançou quinze álbuns de estúdio, quatro álbuns ao vivo, dois EP e cinco compilações.",
 	birthday: "01/01/1983",
 	country: "Estados Unidos",
+	image: open("public/img/band/megadeth.jpg"),
 	artists: [
 		Artist.new(
 			name: "David Mustaine",
-			biography: "onhecido como Dave Mustaine, é vocalista, guitarrista e líder da banda de thrash metal Megadeth, a qual fundou após ter sido demitido do Metallica, devido aos seus constantes problemas com drogas e álcool. Dave é um dos pioneiros do estilo thrash/speed metal no mundo, sendo um grande e aclamado ícone tanto para esses gêneros, quanto para o rock em geral.",
+			biography: "Conhecido como Dave Mustaine, é vocalista, guitarrista e líder da banda de thrash metal Megadeth, a qual fundou após ter sido demitido do Metallica, devido aos seus constantes problemas com drogas e álcool. Dave é um dos pioneiros do estilo thrash/speed metal no mundo, sendo um grande e aclamado ícone tanto para esses gêneros, quanto para o rock em geral.",
 			birthday: "13/09/1961",
-			country: "Estados Unidos"
+			country: "Estados Unidos",
+			image: open("public/img/artist/dave-mustaine.jpg")
 		),
 		Artist.new(
 			name: "Kiko Loureiro",
 			biography: "É um músico multi-instrumentista, compositor e guitarrista brasileiro, membro da banda de metal melódico/power metal Angra, e atualmente é membro da banda de thrash metal Megadeth.",
 			birthday: "16/05/1972",
-			country: "Brazil"
+			country: "Brazil",
+			image: open("public/img/artist/kiko-loureiro.jpg")
 		)
 	],
 	albums: [
 		Album.new(
 			name: "Killing Is My Business... and Business Is Good!",
-			release_year: "1985"
+			release_year: "1/1/1985",
+			image: open("public/img/album/killing-is-my-business.jpg")
 		),
 		Album.new(
 			name: "Peace Sells... but Who's Buying?",
-			release_year: "1986"
+			release_year: "1/1/1986",
+			image: open("public/img/album/peace-sells.jpg")
 		),
 		Album.new(
 			name: "So Far, So Good... So What!",
-			release_year: "1988"
+			release_year: "1/1/1988",
+			image: open("public/img/album/so-far-so-good.jpg")
 		),
 		Album.new(
 			name: "Rust in Peace",
-			release_year: "1990"
+			release_year: "1/1/1990",
+			image: open("public/img/album/rust-in-peace.jpg")
 		),
 		Album.new(
 			name: "Countdown to Extinction",
-			release_year: "1992"
+			release_year: "1/1/1992",
+			image: open("public/img/album/countdown-to-extinction.jpg")
 		)
 	]
 ])
